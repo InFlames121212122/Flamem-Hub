0caa489a62d200f9a9ebcdb6805a2dc071191680
InFlames121212122 <109564844+InFlames121212122@users.noreply.github.com>
2022-09-03 10:39:56 +0300

    Flamem Hub script
    Flamem Hub script dad

---
+++ Flamem Hub script.lua
@@ -0,0 +1,35 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Flamem Hub", "DarkTheme")
+
+local Tab = Window:NewTab("Project Slayers")
+
+local Section = Tab:NewSection("Project Slayers")
+
+
+Section:NewButton("Codex Hub", "", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/CodexHubX/CodexHubX/main/Projectnew.md'))()
+end)
+
+local Tab = Window:NewTab("Pls Donete")
+
+local Section = Tab:NewSection("Pls Donete")
+
+Section:NewButton("Afk Auto Farm", "", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/tzechco/roblox-scripts/main/PLS%20DONATE/autofarm.lua'))()
+end)
+
+local Tab = Window:NewTab("Evade")
+
+local Section = Tab:NewSection("Evade")
+
+Section:NewButton("SmoxHub", "", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SmoxHub/SmoxHub/main/Evade"))()
+end)
+
+local Tab = Window:NewTab("Blox Fruits")
+
+local Section = Tab:NewSection("Blox Fruits")
+
+Section:NewButton("Mukuro Hub", "", function()
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/xDepressionx/Free-Script/main/AllScript.lua")()
+end)
